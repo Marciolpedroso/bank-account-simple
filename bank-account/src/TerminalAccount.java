231c00e94dcc4ed3cc3bc60826919252e0768f51
Marcio Pedroso <marcioluizpf@icloud.com>
2026-03-24 09:51:01 +0100

    adding a basic-java-sintax

--- bank-account/src/TerminalAccount.java
+++ bank-account/src/TerminalAccount.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class TerminalAccount {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the Bank Account System.");
+        System.out.println("Please enter your account number:");
+        int accountNumber = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Please enter your agency:"); 
+        String agency = sc.nextLine();
+        
+        System.out.println("Please enter your account holder name:");
+        String accountHolderName = sc.nextLine();
+
+        System.out.println("Please enter your initial balance:");
+        double initialBalance = sc.nextDouble();
+        
+
+        System.out.println("Account created successfully!");
+        System.out.println("Hello " + accountHolderName + "! Thank you for creating an account in our Bank. Your agency is " + agency + " and your account number is " + accountNumber + ". Your balance is already to withdraw: " + initialBalance);
+
+        sc.close();
     }
+
 }
+
